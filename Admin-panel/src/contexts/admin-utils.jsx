540f8ea59bbbd00424ca2431af419f2dd6388459
kaixiang-uoa <kxzhang220@gmail.com>
2025-05-07 18:35:44 +0930

    rebuild admin-panel part

--- Admin-panel/src/contexts/admin-utils.jsx
+++
@@ -1,5 +0,0 @@
-// 创建新文件存放常量和工具函数
-import { createContext } from "react";
-
-// 创建上下文
-export const AdminContext = createContext(null);
--- Admin-panel/src/contexts/useAdminContext.jsx
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { AdminContext } from "./admin-utils";
-
-export function useAdminContext() {
-  return useContext(AdminContext);
-}

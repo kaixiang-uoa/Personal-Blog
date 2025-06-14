f0f7cbf7a5c293f7ff1af5a2cdc556b9ef63f0e5
kaixiang-uoa <kxzhang220@gmail.com>
2025-06-14 22:40:30 +0930

    fix import error

--- admin-panel/components/ErrorBoundary.tsx
+++ admin-panel/components/ErrorBoundary.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/inputs/button";
 
 interface ErrorBoundaryProps {
   children: React.ReactNode;

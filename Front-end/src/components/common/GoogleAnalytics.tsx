e58d6e392ec51f89ad77b0da0cda0a6cc556e128
kaixiang-uoa <kxzhang220@gmail.com>
2025-08-19 08:55:51 +0930

    add: Google Analytics debug logging

--- Front-end/src/components/common/GoogleAnalytics.tsx
+++ Front-end/src/components/common/GoogleAnalytics.tsx
@@ -19,12 +19,22 @@ import { initGA, isGAEnabled } from '@/lib/analytics';
  */
 export function GoogleAnalytics() {
   useEffect(() => {
+    // Debug: Log environment variable status
+    console.log('GA Debug - Environment variable:', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID);
+    console.log('GA Debug - isGAEnabled:', isGAEnabled);
+
     if (isGAEnabled) {
       try {
+        console.log('GA Debug - Initializing Google Analytics...');
         initGA();
+        console.log('GA Debug - Google Analytics initialized successfully');
       } catch (error) {
         console.error('Google Analytics initialization failed:', error);
       }
+    } else {
+      console.warn(
+        'GA Debug - Google Analytics is disabled. Check NEXT_PUBLIC_GA_MEASUREMENT_ID environment variable.'
+      );
     }
   }, []);
 
